5bf4f6354892d128b1291b3a3913965800458231
nowf41 <111325821+nowf41@users.noreply.github.com>
2025-03-22 03:16:10 +0000

    progress commit

--- hrp3/sdk/workspace/wro2025/app.c
+++ hrp3/sdk/workspace/wro2025/app.c
@@ -1,12 +1,6 @@
 #include "ev3api.h"
 #include "app.h"
-#define left_sensor EV3_PORT_1
-#define right_sensor EV3_PORT_4
-#define left_motor EV3_PORT_A
-#define right_motor EV3_PORT_D
-#define color_sensor COLOR_SENSOR
-#define large_motor LARGE_MOTOR
-#define kp 0.5
+#include "ports.h"
 
 int min(int a, int b) { if (a > b) { return b; } else { return a; } }
 int max(int a, int b) { if (a > b) { return a; } else { return b; } }
@@ -14,23 +8,5 @@ int abs(int a) { if (a >= 0) { return a; } else { return -a; } }
 int mid(int a, int b, int c) {if (a>=b&&b>=c){return b;}if (a>=c&&c>=b){return c;}if (b>=a&&a>=c){return a;}if (b>=c&&c>=a){return c;}if (c>=a&&a>=b){return a;}if (c>=b&&b>=a){return b;}return 0;}
 
 void main_task(intptr_t unused) {
-    // initialize
-    ev3_sensor_config(left_sensor, color_sensor);
-    ev3_sensor_config(right_sensor, color_sensor);
-    ev3_motor_config(left_motor, large_motor);
-    ev3_motor_config(right_motor, large_motor);
-
-    int diff = 0;
-    int p = 0;
-    // trace the line until the center button to be pressed
-    while (!ev3_button_is_pressed(ENTER_BUTTON)) {
-        diff = ev3_color_sensor_get_reflect(left_sensor) - ev3_color_sensor_get_reflect(right_sensor);
-        p = diff * kp;
-
-        ev3_motor_set_power(left_motor, mid(-90, -30-p, 90));
-        ev3_motor_set_power(right_motor, mid(-90, -30+p, 90));
-    }
-
-    ev3_motor_stop(left_motor, true);
-    ev3_motor_stop(right_motor, true);
+    
 }
---
+++ hrp3/sdk/workspace/wro2025/ports.h
@@ -0,0 +1,36 @@
+#ifndef PORTS_H
+#define PORTS_H
+
+// === PORTS ===
+
+#define LEFT_MOTOR EV3_PORT_A
+#define RIGHT_MOTOR EV3_PORT_B
+#define CRANE EV3_PORT_C
+#define FINGER EV3_PORT_D
+
+#define LEFT_MOTOR_TYPE LARGE_MOTOR
+#define RIGHT_MOTOR_TYPE LARGE_MOTOR
+#define CRANE_TYPE LARGE_MOTOR
+#define FINGER_TYPE MEDIUM_MOTOR
+
+#define LEFT_COLOR EV3_PORT_1
+#define RIGHT_COLOR EV3_PORT_2
+#define GATE_COLOR EV3_PORT_3
+#define GYRO EV3_PORT_4
+
+#define LEFT_COLOR_TYPE COLOR_SENSOR
+#define RIGHT_COLOR_TYPE COLOR_SENSOR
+#define GATE_COLOR_TYPE COLOR_SENSOR
+#define GYRO_TYPE GYRO_SENSOR
+
+// ===MACHINE INFORMATION===
+
+#define TIRE_RADIUS 22. // milli meter (float)
+#define TIRE_DISTANCE 100. // milli meter (float)
+
+// === CONTROL CONSTANTS ===
+#define P_GAIN 1.0 // float
+// === GENERAL ===
+#define PI 3.14 // float
+
+#endif /* PORTS_H */
