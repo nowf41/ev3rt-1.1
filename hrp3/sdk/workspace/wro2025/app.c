c84f7da2fca76920ecd95ad4d2c9211c08700016
nowf41 <111325821+nowf41@users.noreply.github.com>
2025-04-22 08:00:33 +0900

    Update app.c

--- hrp3/sdk/workspace/wro2025/app.c
+++ hrp3/sdk/workspace/wro2025/app.c
@@ -18,31 +18,32 @@ void main_task(intptr_t unused) {
 
 int port_flags = 0;
 
+// Todo handle error
 void init_ports() {
     ev3_sensor_config(GYRO, GYRO_TYPE);
     ev3_gyro_sensor_reset(GYRO);
-    port_flags += 1 << GYRO_FLAG_DIG;
+    port_flags |= 1 << GYRO_FLAG_DIG;
 
     ev3_motor_config(LEFT_MOTOR, LEFT_MOTOR_TYPE);
-    port_flags += 1 << LEFT_MOTOR_FLAG_DIG;
+    port_flags |= 1 << LEFT_MOTOR_FLAG_DIG;
     
     ev3_motor_config(RIGHT_MOTOR, RIGHT_MOTOR_TYPE);
-    port_flags += 1 << RIGHT_MOTOR_FLAG_DIG;
+    port_flags |= 1 << RIGHT_MOTOR_FLAG_DIG;
 
     ev3_motor_config(FINGER, FINGER_TYPE);
-    port_flags += 1 << FINGER_FLAG_DIG;
+    port_flags |= 1 << FINGER_FLAG_DIG;
     
     ev3_motor_config(CRANE, CRANE_TYPE);
-    port_flags += 1 << CRANE_FLAG_DIG;
+    port_flags |= 1 << CRANE_FLAG_DIG;
     
     ev3_sensor_config(LEFT_COLOR, LEFT_COLOR_TYPE);
-    port_flags += 1 << LEFT_COLOR_FLAG_DIG;
+    port_flags |= 1 << LEFT_COLOR_FLAG_DIG;
     
     ev3_sensor_config(RIGHT_COLOR, RIGHT_COLOR_TYPE);
-    port_flags += 1 << RIGHT_COLOR_FLAG_DIG;
+    port_flags |= 1 << RIGHT_COLOR_FLAG_DIG;
     
     ev3_sensor_config(GATE_COLOR, GATE_COLOR_TYPE);
-    port_flags += 1 << GYRO_FLAG_DIG;
+    port_flags |= 1 << GYRO_FLAG_DIG;
 }
 
 void require_port(int flag_dig) {
